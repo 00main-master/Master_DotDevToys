4f36277c9f93808749e7f884f2dc50158829fa3e
00main-master <00main.master@gmail.com>
2024-08-15 00:05:31 +0700

    Сделанно

---
+++ Master_DotDevToys/FieldVlewer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Spectre.Console;
+
+namespace Master_DotDevToys
+{
+    public static class Viewer
+    {
+        public static int Count = 0;
+        public static void Snapshot<T>(T obj, string? Label = null, 
+            [System.Runtime.CompilerServices.CallerMemberName] string memberName = "",
+            [System.Runtime.CompilerServices.CallerLineNumber] int sourceLineNumber = 0) 
+        {
+            Type type = obj.GetType();
+
+            string s = $"{Count} {type.FullName}/{memberName}:{sourceLineNumber}";
+            Tree tree = new(Label == null ? s : $"{s} - {Label}");
+            
+
+            foreach (var item in type.GetProperties())
+            {
+                TreeNode node = tree.AddNode($"Property: {item.Name} - {item.PropertyType.Name}");
+                node.AddNode(item.GetValue(obj).ToString());
+            }
+            foreach (var item in type.GetFields())
+            {
+                TreeNode node = tree.AddNode($"Field: {item.Name} - {item.FieldType.Name}");
+                node.AddNode(item.GetValue(obj).ToString());
+            }
+            
+
+            AnsiConsole.Write(tree);
+            Count++;
+        }
+    }
+}
--- Master_DotDevToys/Program.cs
+++ Master_DotDevToys/Program.cs
@@ -1,10 +1,17 @@
 ﻿namespace Master_DotDevToys
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public string s { get; set; }
+        static void Main(string[] args) => new Program()._Main();
+        private void _Main() 
         {
-            Console.WriteLine("Hello, World!");
+            s = "ABOBA";
+            Viewer.Snapshot(this);
+            while (true)
+            {
+                Console.WriteLine($"{((int)Console.ReadKey().Key)}");
+            }
         }
     }
 }
